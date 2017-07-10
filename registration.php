b9939adb3475133b531c2f728e80899d7b52ee82
Cuong NQ <bestearnmoney87@gmail.com>
2017-07-11 04:35:27 +0700

    init source code

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @Author: Ngo Quang Cuong
+ * @Date:   2017-07-11 03:54:20
+ * @Last Modified by:   nquangcuong
+ * @Last Modified time: 2017-07-11 03:54:28
+ * @website: http://giaphugroup.com
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'phpcuong_nl_nl',
+    __DIR__
+);
